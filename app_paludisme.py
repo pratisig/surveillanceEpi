a993331dc2d8b0802ac628e61f659897a68d8936
pratisig <37810753+pratisig@users.noreply.github.com>
2026-02-25 13:40:40 +0000

    Update app_paludisme.py

--- app_paludisme.py
+++ app_paludisme.py
@@ -13,6 +13,8 @@ import geopandas as gpd
 import numpy as np
 import folium
 import tempfile
+import zipfile
+import base64
 from folium import Popup, Tooltip, CircleMarker, GeoJson, LayerControl, DivIcon
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
@@ -2795,6 +2797,7 @@ st.markdown("""
     <p>Version 1.0 | Développé avec | Python • Streamlit • GeoPandas • Scikit-learn par Youssoupha MBODJI</p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
